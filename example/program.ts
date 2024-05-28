d64ac542b6c5a44076701a08b59571d4aa09931f
Jeremie Dayan <dayanjeremie@gmail.com>
2024-05-28 15:16:26 +0200

    add program.ts

---
+++ example/program.ts
@@ -0,0 +1,102 @@
+import { Context, Effect, Layer, ManagedRuntime, pipe } from "effect";
+import * as rte from "fp-ts/ReaderTaskEither";
+import type { ReaderTaskEither as RTE } from "fp-ts/ReaderTaskEither";
+
+import {
+  contextToFpts,
+  effectFunctionToFpts,
+  getFptsMapping,
+  portToFpts,
+  type FptsAccess,
+  type FptsConvertible,
+} from "@inato/effect-fpts-interop";
+
+// domain
+class Foo {
+  constructor(readonly id: string) {}
+  static make = () => new Foo("random-id");
+}
+
+// ports & adpaters
+interface FooRepository extends FptsConvertible<"fooRepository"> {
+  getById: (id: string) => Effect.Effect<Foo, Error>;
+  store: (foo: Foo) => Effect.Effect<void, Error>;
+}
+
+interface FooRepositoryAccess extends FptsAccess<FooRepository> {}
+
+const FooRepositoryTag = Context.GenericTag<FooRepository>("FooRepository");
+
+const FooRepositoryFptsMapping = getFptsMapping(
+  FooRepositoryTag,
+  "fooRepository"
+);
+
+const FooRepository = Effect.serviceFunctions(FooRepositoryTag);
+
+const FooRepositoryFpts = portToFpts(FooRepository, FooRepositoryFptsMapping);
+
+declare const FooRepositoryLive: Layer.Layer<FooRepository>;
+
+interface TransformFooService extends FptsConvertible<"transformFooService"> {
+  transform: (foo: Foo) => Effect.Effect<Foo, Error>;
+}
+
+interface TransformFooServiceAccess extends FptsAccess<TransformFooService> {}
+
+const TransformFooServiceTag = Context.GenericTag<TransformFooService>(
+  "TransformFooService"
+);
+
+const TransformFooServiceFptsMapping = getFptsMapping(
+  TransformFooServiceTag,
+  "transformFooService"
+);
+
+const TransformFooService = Effect.serviceFunctions(TransformFooServiceTag);
+
+const TransformFooServiceFpts = portToFpts(
+  TransformFooService,
+  TransformFooServiceFptsMapping
+);
+
+declare const TransformFooServiceLive: Layer.Layer<TransformFooService>;
+
+// usecases
+export const createFooUseCase: RTE<FooRepositoryAccess, Error, Foo> = pipe(
+  rte.of(Foo.make()),
+  rte.tap(FooRepositoryFpts.store)
+);
+
+export const transformFooUseCase = (id: string) =>
+  FooRepository.getById(id).pipe(
+    Effect.flatMap(TransformFooService.transform),
+    Effect.flatMap(FooRepository.store)
+  );
+
+export const transformFooUseCaseFpts = effectFunctionToFpts(
+  transformFooUseCase,
+  {
+    ...TransformFooServiceFptsMapping,
+    ...FooRepositoryFptsMapping,
+  }
+);
+
+// program
+const main = async () => {
+  const runtime = ManagedRuntime.make(
+    Layer.mergeAll(FooRepositoryLive, TransformFooServiceLive)
+  );
+
+  await runtime.runPromise(transformFooUseCase("my-foo-id"));
+
+  const { context } = await runtime.runtime();
+
+  const services = contextToFpts(context, {
+    ...TransformFooServiceFptsMapping,
+    ...FooRepositoryFptsMapping,
+  });
+
+  await createFooUseCase(services)();
+};
+main();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export const foo = 1;
+export * from "./contextToFpts.js";
+export * from "./effectFunctionToFpts.js";
+export * from "./FptsConvertible.js";
+export * from "./getFptsMapping.js";
+export * from "./portToEffect.js";
+export * from "./portToFpts.js";
