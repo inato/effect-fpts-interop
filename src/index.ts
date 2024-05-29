c0994cae19b78b036644d9575fe13f83dd48f9b3
Jeremie Dayan <dayanjeremie@gmail.com>
2024-05-29 15:57:25 +0200

    remove export of effectFunctionToFpts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 export * from "./contextToFpts.js";
-export * from "./effectFunctionToFpts.js";
 export * from "./FptsConvertible.js";
 export * from "./getFptsMapping.js";
 export * from "./portToEffect.js";
