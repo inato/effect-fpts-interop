acc64fa2f5b776a135419f603b80607f0940526b
Jeremie Dayan <dayanjeremie@gmail.com>
2024-05-30 11:39:18 +0200

    export new helpers

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 export * from "./contextToFpts.js";
 export * from "./FptsConvertible.js";
+export * from "./functionToEffect.js";
+export * from "./functionToFpts.js";
 export * from "./getFptsMapping.js";
 export * from "./portToEffect.js";
 export * from "./portToFpts.js";
